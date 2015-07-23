166bbe149fb2037f0173828e2992092a35d235e9
Brian Chan <brian.chan@liferay.com>
2015-07-23 13:35:21 -0700

    LPS-56580 left over

--- modules/apps/service-access-policy/service-access-policy-service/src/META-INF/sql/indexes.sql
+++ modules/apps/service-access-policy/service-access-policy-service/src/META-INF/sql/indexes.sql
@@ -1,5 +1,2 @@
-create index IX_A038DF8A on SACPEntry (companyId, name);
-create index IX_93856CB1 on SACPEntry (uuid_, companyId);
-
 create index IX_D6FCBA16 on ServiceAccessPolicy (companyId, name);
 create index IX_353CE3A5 on ServiceAccessPolicy (uuid_, companyId);
--- modules/apps/service-access-policy/service-access-policy-service/src/META-INF/sql/tables.sql
+++ modules/apps/service-access-policy/service-access-policy-service/src/META-INF/sql/tables.sql
@@ -1,17 +1,3 @@
-create table SACPEntry (
-	uuid_ VARCHAR(75) null,
-	sacpEntryId LONG not null primary key,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	allowedServiceSignatures STRING null,
-	defaultSACPEntry BOOLEAN,
-	name VARCHAR(75) null,
-	title STRING null
-);
-
 create table ServiceAccessPolicy (
 	uuid_ VARCHAR(75) null,
 	serviceAccessPolicyId LONG not null primary key,
